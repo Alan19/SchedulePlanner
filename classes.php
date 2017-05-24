68587e7bc408a5d55f9cbd7c940f341ae780ea62
Alan Xiao <alanxiao44@gmail.com>
2017-05-23 22:55:46 -0400

    Added new images and improved header

--- classes.php
+++ classes.php
@@ -11,28 +11,44 @@
     <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
     <script>
         function initialize(){
-
+            courses = [new Course('English 101', 'English'), new Course('Calculus 101', 'Math')];
+        }
+        function Course(name, subject){
+            this.name = name;
+            this.subject = subject;
         }
     </script>
 </head>
 
 <body onload="initialize()">
-    <nav class="light-blue lighten-1" role="navigation">
-        <div class="nav-wrapper container">
-            <a id="logo-container" href="index.html" class="brand-logo">Schedule Planner</a>
-            <ul class="right hide-on-med-and-down">
-                <li><a href="classes.php">Classes</a>
-                </li>
-            </ul>
-            <ul id="nav-mobile" class="side-nav">
-                <li><a href="classes.php">Classes<i class="large material-icons">schedule</i></a></li>
-            </ul>
-            <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-        </div>
-    </nav>
+    <header>
+        <nav class="light-blue lighten-1" role="navigation">
+            <div class="nav-wrapper container">
+                <a id="logo-container" href="index.html" class="brand-logo left">Schedule Planner</a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="#" data-activates="nav-mobile" class="show-on-large button-collapse"><i class="material-icons">menu</i></a></li>
+                    <li><a href="classes.php">Classes</a></li>
+                </ul>
+                <ul id="nav-mobile" class="side-nav">
+                    <li>
+                        <div class="userView">
+                            <div class="background"></div>
+                            <a href="#!user"><img class="circle" src="resources\student1.png"></a>
+                            <a href="#!name"><span class="white-text name">John Doe</span></a>
+                            <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+                        </div>
+                    </li>
+                    <li><a href="CollegePlanner.php">Home<i class="large material-icons">home</i></a></li>
+                    <li><a href="CollegePlanner.php">Courses<i class="large material-icons">class</i></a></li>
+                    <li><a href="classes.php">Schedule<i class="large material-icons">schedule</i></a></li>
+                </ul>
+               
+            </div>
+        </nav>
+    </header>
     <main>
         <div class="container">
-            <table class="bordered responsive-table">
+            <table class="bordered responsive-table"></table>
         </div>
     </main>
     <footer class="page-footer light-blue darken-1">
