4035a6c919569ffbebb46bba7bcc4dae1c293e16
2MSBHVE <max@miku.nyc>
2017-06-15 16:46:37 -0400

    clear.php
    fight

---
+++ clear.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+</style>
+<script>
+function deleteAllCookies() {
+	var cookies = document.cookie.split(";");
+
+	for (var i = 0; i < cookies.length; i++) {
+		var cookie = cookies[i];
+		var eqPos = cookie.indexOf("=");
+		var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+	}
+}
+
+function goHome(){
+	document.location.href = "/";
+}
+
+</script>
+</head>
+<body onload="deleteAllCookies();goHome();">
+</body>
+</html>
